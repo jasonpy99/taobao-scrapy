2e5ef154138a28d4586ab1c3e3e67b4c47164123
weihua <175990271@qq.com>
2018-06-03 18:10:12 +0800

    1

--- test.py
+++ test.py
@@ -43,15 +43,42 @@ def fabu(url):
     type_list = html.xpath('//h1[@class="title-txt"]//span[@class="title-tag"]/text()')
     type = ''
     for i in type_list:
-        type = str(i) + '\n' + str(type)
-    type = re.sub(r'\n$','',type)
+        type = str(type) + '\n' + str(i)
+    type = re.sub(r'^\n','',type)
     print(type)
     post.custom_fields.append({
         'key':'type',
         'value':type
     })
 
-    post.id = wp.call(posts.NewPost(post))
-    print(str(base_url) + '/?p=' + str(post.id))
+    # 卖点
+    maidian_list = html.xpath('//div[@class="item-subtitle-wrap"]/ul//li/text()')
+    maidian = ''
+    for i in maidian_list:
+        maidian = str(maidian) + '\n' +str(i)
+    maidian = re.sub(r'^\n', '', maidian)
+    print(maidian)
+    post.custom_fields.append({
+        'key':'maidian',
+        'value':maidian
+    })
 
-fabu('https://traveldetail.fliggy.com/item.htm?id=543815490670')
+    # 原价
+    yuanjia_list = html.xpath('//div[@class="item-price"]/div[@class="price-items"]/dl[@class="price-dl"]/dd[@class="price-content"][1]//text()')
+    yuanjia = ''
+    for i in yuanjia_list:
+        yuanjia = str(yuanjia) + str(i)
+    print(yuanjia)
+
+    # 现价
+    xianjia_list = html.xpath('//div[@class="item-price"]/div[@class="price-items"]/dl[@class="price-dl"]/dd[@class="price-content"][2]//text()')
+    xianjia = ''
+    for i in xianjia_list:
+        xianjia = str(xianjia)+str(i)
+    print(xianjia)
+
+    # 返回文章ID
+    # post.id = wp.call(posts.NewPost(post))
+    # print(str(base_url) + '/?p=' + str(post.id))
+
+fabu('https://traveldetail.fliggy.com/item.htm?spm=181.7621407.a1z9b.6.77ca1e3a1fi35e&id=546636958919&scm=20140635.1_1_2.0.0b83e0e715280164660381559e1a9b')
